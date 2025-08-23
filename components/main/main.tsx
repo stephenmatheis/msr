a2df6e0139c7731b3e7a14067a2f9cb9637ae293
Stephen Matheis <stephen.a.matheis@gmail.com>
2025-08-23 14:36:11 -0400

    fix: main animation on toggle sidebar

--- components/main/main.tsx
+++ components/main/main.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import * as motion from "motion/react-client";
 import { useUI } from "@/providers/ui-provider";
 import styles from "./main.module.scss";
 
@@ -8,11 +9,28 @@ type MainProps = {
 };
 
 export function Main({ children }: MainProps) {
-    const { isSidebarOpen, setIsSidebarOpen } = useUI();
+    const { isSidebarOpen } = useUI();
 
     return (
-        <div className={`${styles.main} ${isSidebarOpen ? styles.open : styles.closed}`}>
+        <motion.div
+            className={styles.main}
+            variants={{
+                open: {
+                    paddingLeft: 350,
+                },
+                closed: {
+                    paddingLeft: 16,
+                },
+            }}
+            initial="open"
+            animate={isSidebarOpen ? "open" : "closed"}
+            transition={{
+                type: "spring",
+                stiffness: 300,
+                damping: 20,
+            }}
+        >
             <div className={styles.content}>{children}</div>
-        </div>
+        </motion.div>
     );
 }
